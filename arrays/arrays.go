039e463a01d35c9e9a46d1f482aa1d2c00622782
cedric duvillier <cduviilier@zbo.media>
2022-07-21 11:48:32 +0200

    test push

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -25,3 +25,5 @@ func main() {
 // An array's length is part of its type, so arrays
 // cannot be resized. This seems limiting, but don't
 // worry; Go provides a convenient way of working with arrays.
+
+// test
